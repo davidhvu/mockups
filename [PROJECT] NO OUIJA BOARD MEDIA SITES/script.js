1694968781e9a05b75529af490618f41e89ba14e
David Vu <dvu4911@gmail.com>
2020-10-23 12:11:22 -0700

    adjusted filter, still not working

--- [PROJECT] NO OUIJA BOARD MEDIA SITES/script.js
+++ [PROJECT] NO OUIJA BOARD MEDIA SITES/script.js
@@ -2,13 +2,22 @@
 
 let movies = ["Hereditary", "Veronica",  "Ouija", "Ouija: Origins of Evil", "The Exorcist", "Witchboard", "Conjuring 2", "What Lies Beneath", "Paranormal Activity", "The Pact", "Ouija House"]
 
+let moviesLowerCase = movies.map(title => title.toLowerCase())
+console.log(moviesLowerCase)
+
+function filterMovie(movieArr,userInput) {
+    movieArr.filter(function (el) {
+        return el.toLowerCase().indexOf(userInput.toLowerCase()) !== -1
+    })
+}
+
+console.log(filterMovie(moviesLowerCase, "ouija"))
 
 const searchBtn = document.getElementById('searchBtn')
 searchBtn.addEventListener('click', function() {
-    const searchTerm = document.getElementById('searchTerm')
-    console.log(searchTerm.value)
-    console.log(movies.filter(function() {
-        return searchTerm.toLowerCase()
-    }))
+    const searchTerm = document.getElementById('searchTerm').value
+    console.log(searchTerm)
+    console.log(filterMovie(movies, searchTerm))
+    
 })
 
