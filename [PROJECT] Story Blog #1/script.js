57dcee951d7eb98dfef61dde8503d4ff29045672
David Vu <dvu4911@gmail.com>
2020-11-06 10:14:28 -0800

    added more nav and fixed displays of sects

--- [PROJECT] Story Blog #1/script.js
+++ [PROJECT] Story Blog #1/script.js
@@ -1,10 +1,28 @@
 const secondSectBtn = document.getElementById('second-sect')
-secondSectBtn.addEventListener('click', function() {
-    const displayForm = document.getElementById('d-form')
+secondSectBtn.addEventListener('click', function(e) {
+    const displayForm2 = document.getElementById('d-form-2')
     
-    if (displayForm.style.display == 'block') {
-        displayForm.style.display = 'none'
+    
+    if (displayForm2.style.display == 'block') {
+        displayForm2.style.display = 'none'
+        e.preventDefault()
     } else { 
-        displayForm.style.display = 'block'
+        displayForm2.style.display = 'block'
+        e.preventDefault()
+    }
+
+    
+})
+
+const thirdSectBtn = document.getElementById('third-sect')
+thirdSectBtn.addEventListener('click', function(e) {
+    const displayForm3 = document.getElementById('d-form-3')
+
+    if (displayForm3.style.display == 'block') {
+        displayForm3.style.display = 'none'
+        e.preventDefault()
+    } else { 
+        displayForm3.style.display = 'block'
+        e.preventDefault()
     }
 })
