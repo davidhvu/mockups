c49efef4e0c68077ffd475fc82fa3e7ccd5a7bc8
David Vu <dvu4911@gmail.com>
2020-11-09 10:38:15 -0800

    adjusted tabs to display only one at a time

--- [PROJECT] Story Blog #1/script.js
+++ [PROJECT] Story Blog #1/script.js
@@ -1,12 +1,33 @@
+const firstSectBtn = document.getElementById('first-sect')
 const secondSectBtn = document.getElementById('second-sect')
-secondSectBtn.addEventListener('click', function(e) {
-    const displayForm2 = document.getElementById('d-form-2')
+const thirdSectBtn = document.getElementById('third-sect')
+const displayForm1 = document.getElementById('d-form-1')
+const displayForm2 = document.getElementById('d-form-2')
+const displayForm3 = document.getElementById('d-form-3')
+
+firstSectBtn.addEventListener('click', function(e) {
     
+    if (displayForm1.style.display == 'block') {
+        displayForm1.style.display = 'none'
+        e.preventDefault()
+    } else { 
+        displayForm3.style.display = 'none'
+        displayForm2.style.display = 'none'
+        displayForm1.style.display = 'block'
+        e.preventDefault()
+    }
+
+    
+})
+
+secondSectBtn.addEventListener('click', function(e) {
     
     if (displayForm2.style.display == 'block') {
         displayForm2.style.display = 'none'
         e.preventDefault()
     } else { 
+        displayForm3.style.display = 'none'
+        displayForm1.style.display = 'none'
         displayForm2.style.display = 'block'
         e.preventDefault()
     }
@@ -14,15 +35,18 @@ secondSectBtn.addEventListener('click', function(e) {
     
 })
 
-const thirdSectBtn = document.getElementById('third-sect')
 thirdSectBtn.addEventListener('click', function(e) {
-    const displayForm3 = document.getElementById('d-form-3')
 
     if (displayForm3.style.display == 'block') {
         displayForm3.style.display = 'none'
         e.preventDefault()
     } else { 
+        
+        displayForm2.style.display = 'none'
+        displayForm1.style.display = 'none'
         displayForm3.style.display = 'block'
         e.preventDefault()
     }
 })
+
+const backTopBtn = document.getElementById('backTopBtn')
